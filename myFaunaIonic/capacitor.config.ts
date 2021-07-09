ced478c9504b3f45d89b26b801e8c998a000ea63
alvaro <alvaromb8@gmail.com>
2021-07-09 15:30:06 +0200

    App Ionic

---
+++ myFaunaIonic/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myFaunaIonic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
